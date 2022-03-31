19d1bfc4cd0db120af1cd3594e91aad9e91b6429
Jualhalo <juho.a.halonen@gmail.com>
2022-03-31 16:33:37 +0300

    korjaus2

--- src/components/__tests__/TodoList.test.js
+++ src/components/__tests__/TodoList.test.js
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TodoList from '../Todolist';
 
 test('should render TodoList component', () => {
